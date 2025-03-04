c5c3d00ba684a22903727d45593d9e23213fc285
Waithera <stacywaithera@DESKTOP-E0D09HN>
2025-03-04 14:03:07 +0300

    event listener

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
-}
+    // Select the button element by ID
+    const input = document.getElementById('button');
+  
+    // Define the callback function
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    // Attach the event listener
+    input.addEventListener('click', clickAlert);
+  }
+  
